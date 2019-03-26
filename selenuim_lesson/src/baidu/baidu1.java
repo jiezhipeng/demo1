89fa690883014ece17aed1a4b7589d2dcded7b52
jiezhipeng <2298973340@qq.com>
2019-03-26 15:40:46 +0800

    jd

---
+++ selenuim_lesson/src/baidu/baidu1.java
@@ -0,0 +1,20 @@
+package baidu;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class baidu1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		  System.setProperty("webdriver.chrome.driver",".//Tools//chromedriver.exe");
+		  WebDriver driver=new ChromeDriver();
+		  driver.get("http://www.baidu.com");
+		  Thread.sleep(3000);
+		  driver.findElement(By.id("kw")).sendKeys("selenium");
+		  Thread.sleep(3000);
+		  driver.findElement(By.id("su")).click();
+	}
+
+}
